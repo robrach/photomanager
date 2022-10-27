59380e9e96a5aec418ac78b06005d910cdfc9db0
Robert <robert.rachubinski@gmail.com>
2022-10-27 21:46:49 +0200

    Models created

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Album(models.Model):
+
+    def __str__(self):
+        return f'Album_{self.pk}'
+
+
+class Photo(models.Model):
+    title = models.CharField(max_length=100)
+    album_id = models.ForeignKey(Album, on_delete=models.PROTECT)
+    width = models.IntegerField()
+    height = models.IntegerField()
+    dominant_color = models.CharField(max_length=10)
+    url_local = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'Photo_{self.pk}'
