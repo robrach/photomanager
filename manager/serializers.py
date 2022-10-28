9c993145cf451bb8349baf1eb49144b72e0bdd6b
Robert <robert.rachubinski@gmail.com>
2022-10-28 19:03:18 +0200

    serializers.py update (now shows album_id but not ideally)

--- manager/serializers.py
+++ manager/serializers.py
@@ -2,25 +2,24 @@ from .models import Photo, Album
 from rest_framework import serializers
 
 
+class AlbumSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Album
+        fields = ['id']
+
+
 class PhotoSerializer(serializers.HyperlinkedModelSerializer):
+
+    album_id = AlbumSerializer(read_only=True)
 
     class Meta:
         model = Photo
         fields = [
             'id',
             'title',
-            # 'album_id',
+            'album_id',
             'width',
             'height',
             'dominant_color',
             'url_local'
         ]
-        # extra_kwargs = {
-        #     'album_id': {view_name}
-        # }
-
-
-class AlbumSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Album
-        fields = ['id', '__str__']
