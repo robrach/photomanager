4827989110a31b1e7d20cf34ae08ff25502d4963
Robert <robert.rachubinski@gmail.com>
2022-10-30 00:01:01 +0200

    Tests for Api written.

--- manager/tests.py
+++ manager/tests.py
@@ -1,3 +1,61 @@
 from django.test import TestCase
+from rest_framework import status
+import requests
 
-# Create your tests here.
+
+class PhotoListApiTest(TestCase):
+    """
+    Tests of API view: photo_list
+    """
+    def test_get_all(self):
+        response = requests.get('http://127.0.0.1:8000/zdjecia')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        json = response.json()
+        self.assertEqual(type(json), list)
+        if json:
+            self.assertEqual(type(json[0]), dict)
+
+    def test_post_new(self):
+        content = {
+            "title": "example title",
+            "album_id": 1,
+            "url": "/example/url/sting"
+        }
+        response = requests.post('http://127.0.0.1:8000/zdjecia', json=content)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        json = response.json()
+        title = json['title']
+        self.assertEqual(title, 'example title')
+
+
+class PhotosDetailApiTest(TestCase):
+    """
+    Tests of API view: photo_detail
+    """
+    def test_1_get_photo(self):
+        response = requests.get('http://127.0.0.1:8000/zdjecia')
+        global id
+        id = (response.json()[-1]['id'])
+        response = requests.get(f'http://127.0.0.1:8000/zdjecia/{id}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        global json
+        json = response.json()
+        self.assertEqual(type(json), dict)
+
+    def test_2_put_photo(self):
+        json['title'] = 'new different title'
+        response_put = requests.put(f'http://127.0.0.1:8000/zdjecia/{id}', json=json)
+        self.assertEqual(response_put.status_code, status.HTTP_200_OK)
+
+        response_get = requests.get(f'http://127.0.0.1:8000/zdjecia/{id}')
+        self.assertEqual(response_get.status_code, status.HTTP_200_OK)
+
+        title = response_get.json()['title']
+        self.assertEqual(title, 'new different title')
+
+    def test_3_delete_photo(self):
+        response = requests.delete(f'http://127.0.0.1:8000/zdjecia/{id}')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
